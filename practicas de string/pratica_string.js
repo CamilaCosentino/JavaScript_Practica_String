dd94a93d268c308a1057204041ac753c739becd9
CamilaCosentino <89109209+CamilaCosentino@users.noreply.github.com>
2021-08-31 11:34:00 -0300

    Primer Commit

---
+++ practicas de string/pratica_string.js
@@ -0,0 +1,16 @@
+"javascript"[2]; //esto va a devolver "V";
+
+"javascript".length; //esto va a devolver 10
+"javascript".indexOf("script"); //devuelve solo la parte de script
+
+// PARA CREAR STRING
+const string = "esto es un string";
+// o
+const string2 =  new String ("Esto es otro string");
+
+//COMO MANDAR PARAMETRO POR SEPARADO EN UN STRING
+var date = new Date;
+date = date.toString();
+date = date.split(" ")
+date[5];
+console.log(date);
